1ee39649c522f5101fccc36e78233c87f9fc1724
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2025-03-05 11:40:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    let slider=document.querySelector(".slider")
-    let switchInput=document.querySelector("input[type='checkbox']")
+document.addEventListener("DOMContentLoaded", () => {
+  let switchInput = document.querySelector(".switch input"); 
+  let body = document.body;
+  let textContainer1TextArea = document.querySelector(".textContainer1 textarea");
+  let bold=document.querySelector("#bold")
+  let theme = localStorage.getItem("theme");
 
-    switchInput.addEventListener('change',()=>{
-        if (this.checked) {
-            console.log("Checked")
-        } else {
-            console.log("Not changed")
-        }
-    })
-})
+  if (theme === "nightmode") {
+    body.classList.add("nightmode");
+    body.classList.remove("lightmode");
+    switchInput.checked = true;
+  } else {
+    body.classList.add("lightmode");
+    body.classList.remove("nightmode");
+    switchInput.checked = false;
+  }
+
+  switchInput.addEventListener("change", (e) => {
+    e.preventDefault();
+    if (e.target.checked) {
+      body.classList.add("nightmode");
+      body.classList.remove("lightmode");
+      localStorage.setItem("theme", "nightmode");
+      console.log("darkmode");
+    } else {
+      body.classList.remove("nightmode");
+      body.classList.add("lightmode");
+      localStorage.setItem("theme", "lightmode");
+      console.log("lightmode");
+    }
+  });
+
+
+});
